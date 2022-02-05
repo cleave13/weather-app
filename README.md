a718a9dc9fd5d57ed9a96fbe79bfaa5ab2505093
cleave13 <94774869+cleave13@users.noreply.github.com>
2022-02-04 22:04:27 -0700

    completed all HTML, CSS and Javascript plus README.md

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# weather-app
-Weather application for practice with server-side APIs
+# Weather Application
+
+## Description
+
+This application allows its users to search for the name of a city and see the current weather as well as a five-day forecast.
+
+It leverages the One Call free API and was my first experience calling out to a third party API.
+
+This exercise was a lot of fun and it helped me get more familiar with the fetch function as well as API endpoints, URL parameters, and API keys.
+
+## Installation
+
+This application does not need to be installed. The live application can be found [here](https://cleave13.github.io/weather-app/).
+
+The source code can also be accessed [here](https://github.com/cleave13/weather-app).
+
+## Usage
+
+To use this application, navigate to the main screen and search for a city name. When you clik "search" the app will callout to the One Call weather API, retrieve the weather data and display it for the user.
+
+![Weather App Screenshot](./assets/images/weather-app.png)
+
+## Credits
+
+I used Bootstrap version 4.3.1 for the CSS styling and many of the components. See the documentation [here](https://getbootstrap.com/docs/4.3/getting-started/introduction/).
+
+I also leveraged the [One Call API](https://openweathermap.org/) for all of the weather callouts.
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,73 +1,176 @@
 //VARIABLES
 
+//Select all DOM elements and store as variables.
 const searchBtnEl = document.getElementById('search');
-const cityButtons = document.getElementById('city-buttons');
+const cityButtonsEl = document.getElementById('city-buttons');
+const currentCityEl = document.getElementById('current-city');
+const currentTempEl = document.getElementById('current-temp');
+const currentWindEl = document.getElementById('current-wind');
+const currentHumEl = document.getElementById('current-hum');
+const currentUviEl = document.getElementById('current-uvi');
+const cardContainerEl = document.getElementById('card-container');
+
+//declare application variables and default city.
 let savedCities = [];
+let cityName = 'Denver';
 
 //FUNCTIONS
 
+//Retrieve saved cities from local storage and create a button element for each.
 function getSavedCities() {
     const cityList = localStorage.getItem('cities');
-    
     if (cityList) {
-        
         savedCities = JSON.parse(cityList);
         savedCities.forEach(savedCity => {
-
             const cityButtonEl = document.createElement('button');
-
             cityButtonEl.setAttribute('class', 'btn');
             cityButtonEl.setAttribute('class', 'btn-secondary');
             cityButtonEl.setAttribute('class', 'btn-block');
             cityButtonEl.setAttribute('data-city', savedCity);
             cityButtonEl.textContent = savedCity;
-            cityButtons.appendChild(cityButtonEl);
+            cityButtonsEl.appendChild(cityButtonEl);
         });
     }
     return;
 }
 
+//Adds a city to the saved city list as long as it hasn't been previously searched.
 function addCity(city) {
-
-    const cityButtonEl = document.createElement('button');
+    if (JSON.stringify(savedCities).includes(city)) {
+        return;
+    }
     
+    const cityButtonEl = document.createElement('button');
     savedCities.push(city);
-
     cityButtonEl.setAttribute('class', 'btn');
     cityButtonEl.setAttribute('class', 'btn-secondary');
     cityButtonEl.setAttribute('class', 'btn-block');
     cityButtonEl.setAttribute('data-city', city);
     cityButtonEl.textContent = city;
-    cityButtons.appendChild(cityButtonEl);
-
+    cityButtonsEl.appendChild(cityButtonEl);
     localStorage.setItem('cities', JSON.stringify(savedCities));
 };
 
-function triggerCallout(requestedCity) {
+//Allows the user to click a button with the city name and populate the weather data for that city.
+function setCityNameFromButton(event) {
+    const target = event.target;
+    cityName = target.getAttribute('data-city');
+    getGeolocation(cityName);
+}
+
+//Sends callout to the geolocation API passing in the city name. Will alert the user that the selected city was not found if callout results in an error.
+function getGeolocation(requestedCity) {
     const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${requestedCity}&limit=1&appid=71efd4d442f298b303ca2c7c46cd4032`
 
-    console.log(geoUrl);
     fetch(geoUrl)
-        .then(function (response) {
-            return response.json();
+        .then(function (geoResponse) {
+            console.log(geoResponse.status)
+            if (geoResponse.status !== 200) {
+                alert('Something went wrong. Please retry your search.')
+            }
+            return geoResponse.json();
         })
-        .then(function (data) {
-            console.log(data);
+        .then(function (geoData) {
+            if (geoData.length > 0) {
+                getWeather(geoData[0].lat, geoData[0].lon)
+            } else {
+                alert('The city name you entered could not be found. Please try a different name.');
+                return;
+            }
+            
         });
 }
 
-function getWeather() {
+//Retrieves the weather data from the geolocation coordinates.
+function getWeather(lat, long) {
+    const weatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutely,hourly,alerts&units=imperial&appid=71efd4d442f298b303ca2c7c46cd4032`
 
+    fetch(weatherUrl)
+        .then(function (weatherResponse) {
+            console.log(weatherResponse.status)
+            if (weatherResponse.status !== 200) {
+                alert('We could not connect to the weather provider. Please retry your search later.')
+            }
+            return weatherResponse.json();
+        })
+        .then(function (weatherData) {
+            if (weatherData) {
+                console.log(weatherData)
+                displayCurrent(weatherData);
+                displayForecast(weatherData);
+            } else {
+                alert('We could not find weather data for that location. Please try again later.');
+                return;
+            }
+            
+        });
+}
+
+// Takes the weather data returned from the API calls and populates the DOM elements for the current day's weather.
+function displayCurrent(cityWeather) {
+    today = new Date();
+    const weatherIcon = document.createElement('img');
+    const iconUrl = 'http://openweathermap.org/img/wn/' + cityWeather.current.weather[0].icon + '@2x.png'
+    weatherIcon.setAttribute('src', iconUrl);
+    
+    currentCityEl.textContent = `${cityName} (${today.getMonth()+1}/${today.getDate()}/${today.getFullYear()})`;
+    currentCityEl.appendChild(weatherIcon);
+
+    currentTempEl.textContent = `${cityWeather.current.temp} ℉`;
+    currentWindEl.textContent = cityWeather.current.wind_speed;
+    currentHumEl.textContent = `${cityWeather.current.humidity}%`;
+    currentUviEl.textContent = cityWeather.current.uvi;
+}
+
+// Takes the weather data returned from the API calls and populates the DOM elements for the forecast.
+function displayForecast(cityForecasts) {
+    for (let i = 0; i < cardContainerEl.children.length; i++) {
+        day = new Date();
+        day.setDate(day.getDate() + i + 1);
+        const cardBody = cardContainerEl.children[i].children[0].children[0];
+        const forecastIcon = document.createElement('img');
+        const forecastIconUrl = 'http://openweathermap.org/img/wn/' + cityForecasts.daily[i].weather[0].icon + '.png';
+        console.log(forecastIcon);
+        forecastIcon.setAttribute('src', forecastIconUrl);
+        const forecastTemp = cityForecasts.daily[i].temp.max;
+        const forecastWind = cityForecasts.daily[i].wind_speed;
+        const forecastHum = cityForecasts.daily[i].humidity;
+        const forecastUvi = cityForecasts.daily[i].uvi;
+
+        cardBody.children[0].textContent = `${day.getMonth()+1}/${day.getDate()}/${day.getFullYear()}`;
+        
+        cardBody.children[1].textContent = `Temp: ${forecastTemp} ℉` ;
+        cardBody.children[1].appendChild(forecastIcon);
+        cardBody.children[2].textContent = `Wind: ${forecastWind} MPH`;
+        cardBody.children[3].textContent = `Humidity: ${forecastHum}%`;
+        cardBody.children[4].textContent = `UVI: ${forecastUvi}`;
+        if (forecastUvi < 3) {
+            cardBody.children[4].setAttribute('class', 'uvi-low');
+        } else if (forecastUvi < 6) {
+            cardBody.children[4].setAttribute('class', 'uvi-mod');
+        } else if (forecastUvi < 8) {
+            cardBody.children[4].setAttribute('class', 'uvi-high');
+        } else {
+            cardBody.children[4].setAttribute('class', 'uvi-high');
+        }
+    }
+    
 }
 
 //EVENT LISTENERS
 
+//Listens for the search button to be clicked and calls the related functions.
 searchBtnEl.addEventListener('click', function () {
-    const cityName = document.getElementById('search-bar');
-    addCity(cityName.value);
-    triggerCallout(cityName.value);
+    cityName = document.getElementById('search-bar').value;
+    addCity(cityName);
+    getGeolocation(cityName);
 });
+
+//Listens for the city buttons to be clicked, then passes in the city name from the button.
+cityButtonsEl.addEventListener('click', setCityNameFromButton);
 
 //LOGIC
 
+//Initializes the application
 getSavedCities();
+getGeolocation('Denver');
